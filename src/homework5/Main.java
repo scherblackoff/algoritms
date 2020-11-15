df33efbe1efd54ec2e3e946cba0908b6fee61a6e
scherblackoff <scherblackoff@gmail.com>
2020-11-15 16:13:31 +0300

    homework5

---
+++ src/homework5/Main.java
@@ -0,0 +1,10 @@
+package homework5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exponentiation exponentiation = new Exponentiation();
+        double a = exponentiation.exponentiation(5,-3);
+        System.out.println(a);
+    }
+}
